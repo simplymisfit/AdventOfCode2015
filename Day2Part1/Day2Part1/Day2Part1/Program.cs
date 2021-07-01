15eaa50de6ca0e93f51ab4c74f6bd49f42a456be
msft <63207978+simplymisfit@users.noreply.github.com>
2021-07-01 19:30:12 +0200

    Day2Part1

--- Day2Part1/Day2Part1/Day2Part1/Program.cs
+++ Day2Part1/Day2Part1/Day2Part1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace Day2Part1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(File.ReadAllLines(@"input.txt")
+            .Select(s => s.Split('x'))
+            .Select(x => x.Select(Int32.Parse))
+            .Select(w => w.OrderBy(x => x).ToArray())
+            .Select(w => 3 * w[0] * w[1] + 2 * w[0] * w[2] + 2 * w[1] * w[2])
+            .Sum());
+
         }
     }
 }
