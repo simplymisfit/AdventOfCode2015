99173f2ecd6f037ad490b84011e7d532bbd11761
msft <63207978+simplymisfit@users.noreply.github.com>
2021-07-04 20:20:37 +0200

    Day3Part1Part2
    x

--- Day3Part1/Day3Part1/Day3Part1/Program.cs
+++ Day3Part1/Day3Part1/Day3Part1/Program.cs
@@ -9,22 +9,52 @@ namespace Day3Part1
 {
     public class Program
     {
+        public int part1(string fileInput)
+        {
+            string input = fileInput;
+            const int columns = 1000, rows = 1000;
+
+            int[,] santaArray = new int[rows, columns];
+            int santaX = columns / 2;
+            int santaY = rows / 2;
+
+            int result = 1;
+
+            santaArray[santaX, santaY] = 1;
+
+            foreach (char c in input)
+            {
+                if (santaArray[santaX, santaY] != 1)
+                {
+                    santaArray[santaX, santaY] = 1;
+                    result += 1;
+                }
+                switch (c)
+                {
+                    case '^':
+                        santaY--;
+                        break;
+                    case 'v':
+                        santaY++;
+                        break;
+                    case '>':
+                        santaX++;
+                        break;
+                    case '<':
+                        santaX--;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+        }
+
         public static void Main(string[] args)
         {
-
-            Console.WriteLine(  
-                File.ReadAllText("input.txt")
-                .Scan(new { x = 0, y = 0 }, (state, c) =>
-                c == '>' ? new { x = state.x + 1, y = state.y } :
-                c == '^' ? new { x = state.x, y = state.y + 1 } :
-                c == '<' ? new { x = state.x - 1, y = state.y } :
-                           new { x = state.x, y = state.y - 1 })
-                .Select(p => String.Format("{0},{1}", p.x, p.y))
-                .GroupBy(p => p)
-                .Count()
-             );
-
-
+            string input = File.ReadAllText("input.txt");
+            Program p = new Program();
+            Console.WriteLine("Part 1: " + p.part1(input));
         }
     }
 }
---
+++ Day3Part2/Day3Part2/Day3Part2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3Part2
+{
+    class Program
+    {
+
+        public int part2(string fileInput)
+        {
+            string input = fileInput;
+            const int columns = 1000, rows = 1000;
+
+            int[,] santaArray = new int[rows, columns];
+            int[,] roboSantaArray = new int[rows, columns];
+            int santaX = columns / 2;
+            int santaY = rows / 2;
+            int roboSantaX = columns / 2;
+            int roboSantaY = rows / 2;
+
+            int turn = 0;
+            int result = 1;
+
+            santaArray[santaX, santaY] = 1;
+            roboSantaArray[roboSantaX, roboSantaY] = 1;
+
+            foreach (char c in input)
+            {
+                if (santaArray[santaX, santaY] != 1)
+                {
+                    santaArray[santaX, santaY] = 1;
+                    roboSantaArray[santaX, santaY] = 1;
+
+                    result += 1;
+                }
+                if (roboSantaArray[roboSantaX,roboSantaY] != 1)
+                {
+                    roboSantaArray[roboSantaX, roboSantaY] = 1;
+                    santaArray[roboSantaX, roboSantaY] = 1;
+
+                    result += 1;
+                }
+
+                switch (c)
+                {
+                    case '^':
+                        if (turn % 2 == 0)
+                            santaY--;
+                        else
+                            roboSantaY--;
+                        break;
+                    case 'v':
+                        if (turn % 2 == 0)
+                            santaY++;
+                        else
+                            roboSantaY++;
+                        break;
+                    case '>':
+                        if (turn % 2 == 0)
+                            santaX++;
+                        else
+                            roboSantaX++;
+                        break;
+                    case '<':
+                        if (turn % 2 == 0)
+                            santaX--;
+                        else
+                            roboSantaX--;
+                        break;
+                    default:
+                        break;
+                }
+                turn += 1;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            Program p = new Program();
+            Console.WriteLine("Part 1: " + p.part2(input));
+        }
+    }
+}
